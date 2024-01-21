f9f776458091348503d46bc1d1d2ad52934d1070
Peter <booanchen@gmail.com>
2024-01-22 00:20:12 +0800

    rebuild and change dist to docs

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: "/chat-app/"
+  publicPath: process.env.NODE_ENV === 'production' ? '/chat-app/' : '/',
 })
